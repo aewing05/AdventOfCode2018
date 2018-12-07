c9f6ca5ff8e324e8ff0ba6252bc85c91d4d9d4a1
Andy Ewing <33497082+aewing05@users.noreply.github.com>
2018-12-06 20:19:48 -0600

    Add files via upload

---
+++ Day1SolutionPython.py
@@ -0,0 +1,14 @@
+import itertools
+
+# Part 1
+data = [int(x) for x in open("Day1Input.txt").readlines()]
+print(sum(data))
+
+# Part 2
+frequency = 0
+frequency_log = {0}
+for num in itertools.cycle(data):
+    frequency += num
+    if frequency in frequency_log:
+        print(frequency); break
+    frequency_log.add(frequency)
